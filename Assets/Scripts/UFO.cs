d87293f961ccbd905dde0326444e17f595d7fa42
Fuenteg1 <Fuenteg1@uci.edu>
2019-02-09 03:01:17 -0800

    ufo spawn

---
+++ Assets/Scripts/UFO.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour {
+
+	public GameObject UFOPrefab;
+	public float spawnnext = 60f;
+	float timePassed = 0f;
+	float travelSpeed = 5f;
+	
+	void Update () {
+		timePassed += Time.deltaTime;
+		if (timePassed > spawnnext){
+			timePassed = 0;
+			if (SpawnNow()){SpawnUFO();}
+		}
+	}
+	void SpawnUFO(){
+		Vector2 position = new Vector2(-9f, 4.2f);
+		GameObject eUFO = Instantiate(UFOPrefab,position, Quaternion.identity);
+		Vector2 currentPosition = eUFO.GetComponent<Rigidbody2D>().velocity;
+		eUFO.GetComponent<Rigidbody2D>().velocity = new Vector2(travelSpeed , currentPosition.y);
+
+	}
+	bool SpawnNow(){
+		float randSpawn = Random.Range(0f,1f);
+		if (randSpawn > .7){return true;}
+		return false;
+	}
+
+	// void OnCollisionEnter2D(Collision2D collision){
+		// if (collision.collider.CompareTag("Ground") || collision.collider.CompareTag("Surprise") || collision.collider.CompareTag("Spike") || collision.collider.CompareTag("pot")|| collision.collider.CompareTag("wall")){
+		// 	Destroy(UFOPrefab);
+		// }
+	// }
+}
---
+++ Assets/Scripts/UFOCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOCollide : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D collision){
+		 if(collision.collider.CompareTag("Projectile") || collision.collider.CompareTag("OuterWall") )
+        {
+            // Kill enemy here
+            Destroy(this.gameObject);
+        }
+	}
+}
